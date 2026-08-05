ee05268bbce01baa641ce759edd367584b0cd76b
drama87g-gif <dr.ama87g@gmail.com>
2026-08-05 11:16:48 +0200

    Add MedicalItem class to core.Models

---
+++ core/Models/MedicalItem.cs
@@ -0,0 +1,9 @@
+namespace Core.Data
+{
+    public class MedicalItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
