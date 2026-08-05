57263bb17a9e5362bc785dd0c0731c40a61fa598
drama87g-gif <dr.ama87g@gmail.com>
2026-08-05 11:15:43 +0200

    Add Patient model class with properties

---
+++ core/Models/patient.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core.Data
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
